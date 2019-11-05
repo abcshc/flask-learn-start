d6a83408cc11e55096cdb760da11e79010d62363
서샛별 <seosaesbyeol@seosaesbyeol-ui-MacBookPro.local>
2019-11-05 21:21:16 +0900

    - pull 연습

--- flask_app/test_start.py
+++ flask_app/test_start.py
@@ -23,14 +23,15 @@ def test_sort_result():
         {"name": "소희철", "count": 5, "time": 4},
         {"name": "이진희", "count": 4, "time": 8},
         {"name": "강경연", "count": 6, "time": 1},
-        {"name": "조성준", "count": 5, "time": 2}
+        {"name": "조성준", "count": 5, "time": 2},
+        {"name": "서샛별", "count": 5, "time": 3}
     ]
     sorted1 = [
         {"name": "이진희", "count": 4, "time": 8},
         {"name": "조성준", "count": 5, "time": 2},
+        {"name": "서샛별", "count": 5, "time": 3},
         {"name": "소희철", "count": 5, "time": 4},
         {"name": "강경연", "count": 6, "time": 1}
-
     ]
     for info in sorted1:
         print(f'이름: {info["name"]}, 횟수: {info["count"]}, 시간: {info["time"]}')
